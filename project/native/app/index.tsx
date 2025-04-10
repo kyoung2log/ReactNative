b58bdc7e56e214a1016108a2d8c9e0e1c79f18fa
devkyoung2 <devkyoung2@gmail.com>
2025-04-10 20:40:16 +0900

    [practice] RN 웹뷰 실습

--- project/native/app/index.tsx
+++ project/native/app/index.tsx
@@ -1,11 +1,13 @@
 // [Section 1]
 // import HelloWorldPage from '@/components/Section 01/01. helloworld';
-import ReactNativeTagsPage from '@/components/Section 01/02. RN tags';
+// import ReactNativeTagsPage from '@/components/Section 01/02. RN tags';
+import WebviewPage from '@/components/Section 01/03. Webview';
 
 const IndexPage = () => {
   return (
     // <HelloWorldPage />
-    <ReactNativeTagsPage />
+    // <ReactNativeTagsPage />
+    <WebviewPage />
   );
 };
 
---
+++ project/native/components/Section 01/03. Webview/index.tsx
@@ -0,0 +1,7 @@
+import { WebView } from 'react-native-webview';
+
+const WebviewPage = () => {
+  return <WebView source={{ uri: 'https://www.naver.com' }} />;
+};
+
+export default WebviewPage;
