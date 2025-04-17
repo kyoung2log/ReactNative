620f13e31c8985e792acf300e29dc55bec11d38d
devkyoung2 <devkyoung2@gmail.com>
2025-04-17 15:16:38 +0900

    [remove] 실습 파일 이동

--- project/native/app/index.tsx
+++ project/native/app/index.tsx
@@ -1,13 +1,14 @@
 // [Section 1]
 // import HelloWorldPage from '@/components/Section 01/01. helloworld';
 // import ReactNativeTagsPage from '@/components/Section 01/02. RN tags';
-import WebviewPage from '@/components/Section 01/03. Webview';
+// import WebviewPage from '@/components/03. Webview';
 
 const IndexPage = () => {
   return (
     // <HelloWorldPage />
     // <ReactNativeTagsPage />
-    <WebviewPage />
+    // <WebviewPage />
+    <></>
   );
 };
 
